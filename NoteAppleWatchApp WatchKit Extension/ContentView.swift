7edf75ad2a5bcb8d3462990ff4ccc76e242fc172
Arashdeep Singh <Arash2348@gmail.com>
2022-06-14 23:28:49 -0700

    Initial
    Initial Commit

---
+++ NoteAppleWatchApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoteAppleWatchApp WatchKit Extension
+//
+//  Created by Jashandeep Singh on 6/14/22.
+//  Copyright © 2022 AJ Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Maharaj!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoteAppleWatchApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  NoteAppleWatchApp WatchKit Extension
+//
+//  Created by Jashandeep Singh on 6/14/22.
+//  Copyright © 2022 AJ Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
